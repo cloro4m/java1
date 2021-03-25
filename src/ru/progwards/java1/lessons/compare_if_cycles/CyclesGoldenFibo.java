2562dcf872a2f60c3761eca13d0da37c9e8d77f5
cloro4m <cloro4m@yandex.ru>
2021-03-25 20:58:31 +0500

    HelloWorld start

--- src/ru/progwards/java1/lessons/compare_if_cycles/CyclesGoldenFibo.java
+++ src/ru/progwards/java1/lessons/compare_if_cycles/CyclesGoldenFibo.java
@@ -27,28 +27,45 @@ public class CyclesGoldenFibo {
             fib1 = fib2;
             fib2 = fib3;
         }
-        return fib3;
+        return fib2;
     }
 
     public static boolean isGoldenTriangle(int a, int b, int c){
-        float A = a, B = b, C = c;
+        float A = a, B = b, C = c, z;
         boolean gold = false;
-        if (a==b) {
-            float z = (C/A);
-            gold = ((z >= 1.61703f)&&(z <= 1.61903f));
-        } else if (b==c) {
-            float z = (A/B);
-            gold = ((z >= 1.61703f)&&(z <= 1.61903f));
-        } else if (a==c) {
-            float z = (B/A);
-            gold = ((z >= 1.61703f)&&(z <= 1.61903f));
+        if ((a == b) || (b == c) || (c == a)){
+            if (a==b) {
+                if(a>c){
+                    z = (A/C);
+                } else {
+                    z = (C/A);
+                }
+                gold = ((z >= 1.61703f)&&(z <= 1.61903f));
+            } else if (b==c) {
+                if(b>a){
+                    z = (B/A);
+                } else {
+                    z = (A/B);
+                }
+                gold = ((z >= 1.61703f)&&(z <= 1.61903f));
+            } else if (a==c) {
+                if(a>b){
+                    z = (A/B);
+                } else {
+                    z = (B/A);
+                }
+                gold = ((z >= 1.61703f)&&(z <= 1.61903f));
+            }
         }
         return gold;
         }
 
     public static void main(String[] args){
         //System.out.println(containsDigit(1234, 2));
-        fiboNumber(15);
+        //System.out.println(fiboNumber(1));
+        //System.out.println(isGoldenTriangle(55,34,34));
+        //fiboNumber(1);
+
         int fib1 = 1, fib2 = 1, fib3 = 0;
         //System.out.print(fib1 + ", "  + fib2+ ", ");
         for(int x = 3; x <= 15; x++){
--- src/ru/progwards/java1/lessons/compare_if_cycles/TriangleInfo.java
+++ src/ru/progwards/java1/lessons/compare_if_cycles/TriangleInfo.java
@@ -3,7 +3,7 @@ package ru.progwards.java1.lessons.compare_if_cycles;
 public class TriangleInfo {
 
     public static boolean isTriangle(int a, int b, int c){
-        boolean side = (a < (b + c)) || (b < (a + c)) || (c < (a + b));
+        boolean side = (a < (b + c)) && (b < (a + c)) && (c < (a + b));
         return side;
     }
 
@@ -18,8 +18,8 @@ public class TriangleInfo {
     }
 
     public static void main(String[] args) {
-        System.out.println(isTriangle(3,3,9));
-        System.out.println(isIsoscelesTriangle(3,3,3));
+        System.out.println(isTriangle(4,4,5));
+        //System.out.println(isIsoscelesTriangle(3,3,3));
     }
 
 }
